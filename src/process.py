fe5b351fea7e647b1abdf525021a30e70271d2d8
lhfbc <lhf_bc@163.com>
2019-04-02 13:48:10 +0800

    use sh to start process

--- src/process.py
+++ src/process.py
@@ -1,8 +1,11 @@
-import os 
 #import subprocess
+import os 
+import sh
+
 
 cur_dir = os.getcwd()
 conf_file= cur_dir + "/scripts/iota_api/conf"
 
 #subprocess.call(["cp", conf_file, conf_file + ".bak"])
-os.popen("cp %s %s" % (conf_file, conf_file + ".bak"))
+#os.popen("cp %s %s" % (conf_file, conf_file + ".bak"))
+sh.cp(conf_file, conf_file + ".bak")
