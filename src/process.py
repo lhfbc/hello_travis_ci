fec1e394e444effb6a2d4c3d3ef2dea7192e288e
lhfbc <lhf_bc@163.com>
2019-04-02 13:38:09 +0800

    modify process

--- src/process.py
+++ src/process.py
@@ -1,6 +1,8 @@
-import os, subprocess
+import os 
+#import subprocess
 
 cur_dir = os.getcwd()
-conf_file= cur_dir + "scripts/iota_api/conf"
+conf_file= cur_dir + "/scripts/iota_api/conf"
 
-subprocess.call(["cp", conf_file, conf_file + ".bak"])
+#subprocess.call(["cp", conf_file, conf_file + ".bak"])
+os.popen("cp %s %s" % (conf_file, conf_file + ".bak"))
